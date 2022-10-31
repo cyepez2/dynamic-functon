618d11b5bb7a31a6845f59cadd3321cdc301c9dd
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2022-10-31 16:41:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#from methods_help import method_help
+#from functions import function
+#from returnStuff import returnS
+from dynamic_functions import dynamic
+
+#method_help()
+#function()
+#returnS()
+dynamic()
