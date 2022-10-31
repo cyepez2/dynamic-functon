fbb51c3d760e692adcac6fb00c0ecfc7b75eb8fa
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2022-10-31 16:47:45 +0000

    found how to check a range and see if it has 3 digits, returning t or f through a function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #from methods_help import method_help
 #from functions import function
 #from returnStuff import returnS
-from dynamic_functions import dynamic
+from dynamic_functions import check_3Digits
 
 #method_help()
 #function()
 #returnS()
-dynamic()
+result = check_3Digits(200)
+print(result)
